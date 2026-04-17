d90f53e4f3edeb510f31d6667ebb9e9801991ade
Mouiezuddin <killedarmouiezuddin@gmail.com>
2026-04-17 11:17:00 +0530

    fix: correct port default and ensure staticfiles dir exists

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,8 @@ import multiprocessing
 import os
 
 
-bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
-workers = int(os.environ.get('WEB_CONCURRENCY', max(2, multiprocessing.cpu_count())))
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+workers = int(os.environ.get('WEB_CONCURRENCY', 1))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 graceful_timeout = int(os.environ.get('GUNICORN_GRACEFUL_TIMEOUT', '30'))
